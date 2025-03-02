2014b77b18b22ac5e2278123b16ed4eb67cff90a
Jeff L <kitermerts@gmail.com>
2025-03-01 20:55:55 -0500

    updates.

--- src/components/AnnouncementBar.tsx
+++ src/components/AnnouncementBar.tsx
@@ -1,10 +1,7 @@
 export default function AnnouncementBar() {
     return (
-        // <div className="w-full bg-blue-500 text-white text-center py-2 text-sm">
-        //     Take a look at our latest blog post! <a href="#" className="underline">The Case for Abstractions in IaC</a>
-        // </div>
-        <div className="bg-blue-600 text-white p-4 text-center">
-    This is a banner!
-</div>
+        <div className="w-full bg-blue-500 text-white text-center py-2 text-sm">
+            Take a look at our latest blog post! <a href="#" className="underline">The Case for Abstractions in IaC</a>
+        </div>
     );
 }
